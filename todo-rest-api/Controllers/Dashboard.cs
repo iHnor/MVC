bebc410d67641c243316fbc46a33107a782d0a9c
Ihor <igormykhno@gmail.com>
2021-11-03 17:50:45 +0200

    Add dashboard

---
+++ todo-rest-api/Controllers/Dashboard.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace todo_rest_api
+{
+    [Route("dashboard")]
+    [ApiController]
+    public class DashboardController:ControllerBase
+    {
+        private TodoItemService todoItemsService;
+        public DashboardController(TodoItemService service)
+        {
+            this.todoItemsService = service;
+        }
+
+        [HttpGet("/today")]
+        public ActionResult TodayTasks()
+        {
+            
+        }  
+    }
+
+}
